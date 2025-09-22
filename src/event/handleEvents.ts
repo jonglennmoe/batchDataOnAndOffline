a05c03171d535d847c5026bef227bd3a73a11d11
jonglennmoe <98458032+jonglennmoe@users.noreply.github.com>
2025-09-22 21:49:01 +0200

    Update handleEvents.ts

--- src/event/handleEvents.ts
+++ src/event/handleEvents.ts
@@ -2,6 +2,7 @@ import { Event } from '../classes/event';
 
 const LOCAL_STORAGE_KEY: string = 'userEvents';
 const NUMBER_OF_EVENTS_BEFORE_BATCH = 5;
+const INTERVAL_BETWEEN_TRYING_TO_BATCH = 1000 * 60;
 
 let events = JSON.parse(window.localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
 
@@ -41,3 +42,6 @@ const sendEventsToApi = () => {
     console.log(error)
   });
 };
+
+batchEvents();
+setInterval(batchEvents, INTERVAL_BETWEEN_TRYING_TO_BATCH);
