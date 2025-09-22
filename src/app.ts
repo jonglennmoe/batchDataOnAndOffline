f61f805ad3578760515f82b0d7e74d8590803f28
jonglennmoe <98458032+jonglennmoe@users.noreply.github.com>
2025-09-22 21:50:10 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,27 +2,44 @@ export {};
 import { Event } from "../src/classes/event";
 import setEvent from "./event/handleEvents";
 
-console.log('BLOP BLOP BLOP')
-
-const draw = (message: string = 'Hello, TypeScript2!') => {
-  console.log(message);
+const draw = () => {
   if (typeof document !== 'undefined') {
-    let heading = document.createElement('p');
-    heading.textContent = message;
-    document.body.appendChild(heading);
-    let link = document.createElement('p');
-    link.textContent = 'Add Event';
-    document.body.appendChild(link);
+
+    let p = document.createElement('p');
+    let link = document.createElement('a');
+    link.setAttribute('href', "javascript:;")
+    link.textContent = 'Add Event Type 1';
+    p.appendChild(link);
+    document.body.appendChild(p);
     link.addEventListener("click", function (e) {
       setEvent({
-        eventName: "clickEvent",
-        eventType: "click"
+        eventName: "Event Type 1",
+        eventType: "clickEvent"
       });
     });
 
+    let p2 = document.createElement('p');
+
+    let link2 = document.createElement('a');
+    link2.setAttribute('href', "javascript:;");
+    link2.textContent = 'Add Event With different data';
+    p2.appendChild(link2);
+    document.body.appendChild(p2);
+    link2.addEventListener("click", function (e) {
+      setEvent({
+        eventType: "Event Something Different",
+        somethingElse: 'Something Else',
+        extraSubdata: {
+          subDataName: 'My subDatadata',
+          subData: {
+            thisCanTake: 'Anything'
+          }
+        },
+        whatDidYouDo: "click"
+      });
+    });
+
+
   }
 };
-
-let event = new Event(JSON.stringify({blop: 'blop'}));
-console.log('event', event);
-draw(event.describe());
+draw();
