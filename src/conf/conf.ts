7208c9847acf9ffa8997b5d92c3b7887c8141226
jonglennmoe <98458032+jonglennmoe@users.noreply.github.com>
2025-09-23 09:34:08 +0200

    removed unused conf file

--- src/conf/conf.ts
+++
@@ -1,9 +0,0 @@
-export const conf = {
-  appName:'samuels online store',
-  expireDate:'2023/30/12',
-  customerName:'samuel l jackson'
-};
-
-const LOCAL_STORAGE_KEY: string = 'userEvents';
-const NUMBER_OF_EVENTS_BEFORE_BATCH: number = 5;
-const INTERVAL_BETWEEN_TRYING_TO_BATCH: number = 1000 * 60;
