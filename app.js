1e8598c924e0976d968ea139e6cf9b91d452fabe
jonglennmoe <98458032+jonglennmoe@users.noreply.github.com>
2025-09-23 15:12:12 +0200

    Added rate limet to express server.

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express');
-const app = express();
-const port = 8081;
+const compression = require('compression');
+const rateLimit =  require('express-rate-limit');
 const path = require('path');
 
+const limiter = rateLimit({
+    windowMs: 3 * 60 * 1000, // 3 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 3 minutes).
+    standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    ipv6Subnet: 62
+});
+
+const app = express();
+const port = 8081;
+
+app.use(limiter)
+app.use(compression());
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req,res) => res.sendFile(__dirname + '/public/index.html'));
